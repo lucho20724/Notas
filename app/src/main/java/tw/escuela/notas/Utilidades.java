0cda317749cdd3d7095ed3ae7debbe8dda52cc45
Luciano Hinojosa <luciano.94.33@gmail.com>
2020-01-13 22:32:14 -0300

    Version 1.1

---
+++ app/src/main/java/tw/escuela/notas/Utilidades.java
@@ -0,0 +1,8 @@
+package tw.escuela.notas;
+
+/**
+ * Created by Lucho on 13/1/2020.
+ */
+
+public class Utilidades {
+}
---
+++ app/src/main/java/tw/escuela/notas/nuevoalumnoActivity.java
@@ -0,0 +1,13 @@
+package tw.escuela.notas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nuevoalumnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevoalumno);
+    }
+}
